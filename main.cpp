bf467e636cad1eec74538dc98ae8315abda7ea6f
Romelianism <Romelianism@gmail.com>
2025-01-09 02:54:17 +0800

    feat: :fire: turn server into basic http server

--- main.cpp
+++ main.cpp
@@ -1,36 +1,33 @@
-#include <algorithm>
-#include <array>
 #include <asio.hpp>
+#include <asio/write.hpp>
 #include <cstdio>
 #include <fmt/base.h>
 #include <fmt/core.h>
 #include <fmt/format.h>
-#include <iostream>
 #include <string>
 
 using asio::ip::tcp;
 
-std::string make_daytime_string() {
-  using namespace std; // For time_t, time and ctime;
-  time_t now = time(0);
-  char buffer[26];
-  ctime_s(buffer, sizeof(buffer), &now);
-  std::string output(buffer);
-  return buffer;
-}
-
 int main(int argc, char *argv[]) {
   try {
     asio::io_context io_context;
-    tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 13));
+    tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 8080));
 
     for (;;) {
       tcp::socket socket(io_context);
       acceptor.accept(socket);
-      std::string message = make_daytime_string();
+
+      const std::string content = "Hello World!";
 
       std::error_code ignored_error;
-      asio::write(socket, asio::buffer(message), ignored_error);
+      std::string http_message = fmt::format("HTTP/1.1 200 OK\n"
+                                             "Content-Type: text/plain\n"
+                                             "Content-Length: {}\n"
+                                             "\n"
+                                             "{}",
+                                             content.length(), content);
+
+      asio::write(socket, asio::buffer(http_message));
     }
   } catch (std::exception &e) {
     fmt::println(stderr, "{}", e.what());
