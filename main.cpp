1cbeb15f042f90a9cd72a9dd711a3058280d3416
Romelianism <Romelianism@gmail.com>
2025-01-08 23:55:38 +0800

    feat: :fire: make server the main file

--- main.cpp
+++ main.cpp
@@ -10,37 +10,27 @@
 
 using asio::ip::tcp;
 
+std::string make_daytime_string() {
+  using namespace std; // For time_t, time and ctime;
+  time_t now = time(0);
+  char buffer[26];
+  ctime_s(buffer, sizeof(buffer), &now);
+  std::string output(buffer);
+  return buffer;
+}
+
 int main(int argc, char *argv[]) {
   try {
-    if (argc != 2) {
-      fmt::println(stderr, "Usage: client <host>");
-      return 1;
-    }
-    fmt::println("host: {}", argv[1]);
     asio::io_context io_context;
-    tcp::resolver resolver(io_context);
-    tcp::resolver::results_type endpoints = resolver.resolve(argv[1], "daytime");
-    std::for_each(
-        endpoints.begin(), endpoints.end(),
-        [&](asio::ip::basic_resolver_entry<tcp> result) {
-          const asio::ip::basic_endpoint<tcp> endpoint = result.endpoint();
-          fmt::println("Endpoint Address: {} Port: {}",
-                       endpoint.address().to_string(), endpoint.port());
-        });
+    tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 13));
 
-    tcp::socket socket(io_context);
-    asio::connect(socket, endpoints);
     for (;;) {
-      std::array<char, 128> buffer;
-      std::error_code error;
+      tcp::socket socket(io_context);
+      acceptor.accept(socket);
+      std::string message = make_daytime_string();
 
-      size_t len = socket.read_some(asio::buffer(buffer), error);
-      if (error == asio::error::eof)
-        break; // Connection closed cleanly by peer.
-      else if (error)
-        throw std::system_error(error); // Some other error.
-
-      std::cout.write(buffer.data(), len);
+      std::error_code ignored_error;
+      asio::write(socket, asio::buffer(message), ignored_error);
     }
   } catch (std::exception &e) {
     fmt::println(stderr, "{}", e.what());
